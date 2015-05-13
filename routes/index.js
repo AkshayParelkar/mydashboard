8d93b3d2ff4ad92db9827610bc63895b37cc3b99
Akshay Parelkar <akshay.parelkar@gmail.com>
2015-05-13 09:43:34 -0700

    implemented a blocking version that pulls top 10 stories, but currently way too slow.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,72 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
+var numhntopstories = 10;
+var requestsync = require('urllib-sync').request;
 
-function updateDashboard() {
-	
+var hnapi = {
+	'topstories': 'https://hacker-news.firebaseio.com/v0/topstories.json',
+	'ask': 'https://hacker-news.firebaseio.com/v0/askstories',
+	'base': 'https://hacker-news.firebaseio.com/v0/item/'
+};
+
+var googleapi = {
+	'gmail': '',
+	'drive': ''
+};
+
+items = {
+	'topstories': [],
+	'ask': [],
+	'gmail': [],
+	'drive': []
+};
+
+/*
+ * items is a JSON object that for storing info on:
+ * hnnews
+ * hnask
+ * gmail
+ * drive
+**/
+function updateDashboard(disp, requestUrl, callback) {
+	// HN API: news
+	var req = https.get(hnapi.topstories, function(res) {
+        var responseString = '';
+    
+        // Buffer the body entirely for processing as a whole
+        res.on('data', function(data) {
+        	//console.log(responseString);
+            responseString += data;
+        });
+
+        res.on('end', function() {
+            callback(responseString, disp);
+        });
+    });
+
+    req.on('error', function(e) {
+        //console.log('problem with request' + e.message);
+    });
+    req.end();
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'my dashboard' });
+	updateDashboard(res, hnapi.topstories, function(storyIds, disp) {
+        var storyIdsJSON = JSON.parse(storyIds); 
+        //console.log('storyIdsJSON'); 
+        //console.log(storyIdsJSON);
+        var storyJSON = [];
+        for (var i = 0; i < 10; i++) {
+        	storyId = storyIdsJSON[i];
+        	var res = requestsync(hnapi.base + JSON.stringify(storyId) + '.json');
+        	//console.log(res.data.toString());
+        	storyJSON.push(JSON.parse(res.data.toString()));
+        };
+		console.log(storyJSON);
+        disp.render('index', { items: storyJSON })
+    });
 });
 
 module.exports = router;
